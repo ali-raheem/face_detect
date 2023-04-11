610cce78f66668006567b36e8b8c927be5f6802f
Ali Raheem <ali.raheem@gmail.com>
2023-04-11 14:06:06 +0100

    Working bboxes

--- face_search.py
+++ face_search.py
@@ -1,41 +1,52 @@
 import os
+import ast
 import sqlite3
 import argparse
 import tkinter as tk
 from tkinter import ttk
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw
 
 num_columns = 5
 display_results = 15
 offset = 0
 
-# Find files with name (from the previous answer)
-def find_files_with_name(database, name):
-    with sqlite3.connect(database) as connection:
-        cursor = connection.cursor()
-        cursor.execute("SELECT filename FROM image_data WHERE people LIKE ? LIMIT ? OFFSET ?",
-                       (f"%{name}%", display_results, offset))
-        result = cursor.fetchall()
-    return [row[0] for row in result]
+def find_files_with_name(database, name):                                                                                           
+    with sqlite3.connect(database) as connection:                                                                                   
+        cursor = connection.cursor()                                                                                                
+        cursor.execute("SELECT filename, people, boxes FROM image_data WHERE people LIKE ? LIMIT ? OFFSET ?",                       
+            (f"%{name}%", display_results, offset))                                                                      
+    result = cursor.fetchall()                                                                                                  
+    parsed_result = []                                                                                                              
+    for row in result:                                                                                                              
+        filename, people_str, boxes_str = row                                                                                       
+        people = ast.literal_eval(people_str) if people_str else None                                                               
+        boxes = ast.literal_eval(boxes_str) if boxes_str else None
+#        print("boxes", boxes)
+        parsed_result.append((filename, people, boxes))                                                                             
+    return parsed_result
 
-# Function to open the image with the default image viewer
-def open_image(filepath):
+def open_image(filepath, names=None, bboxes=None):
     image_window = tk.Toplevel()
     image_window.title(filepath)
     image = Image.open(filepath)
+    print(f"Opening image {filepath} of {names} at {bboxes}.")
+    if all((enable_bbox, names, bboxes)):
+        draw = ImageDraw.Draw(image)
+        for i, bbox in enumerate(bboxes):
+            top, left, bottom, right = bbox
+            draw.rectangle(((left, top), (right, bottom)), outline="blue", width=3)
+            draw.text((left, bottom + 5), names[i], fill="blue")  
     photo = ImageTk.PhotoImage(image)
     image_label = ttk.Label(image_window, image=photo)
     image_label.image = photo
     image_label.pack()
 
-# Function to search for images with the entered name
 def search_images():
     name = name_entry.get().strip()
     print(f"Searching first {display_results} results for {name} offset by {offset}...")
-    filepaths = find_files_with_name(database, name)
-    display_thumbnails(filepaths)
+    thumbnails = find_files_with_name(database, name)
+    display_thumbnails(thumbnails)
 
-# Function to clear the search results
 def reset_search():
     global offset
     offset = 0
@@ -52,13 +63,13 @@ def prev_page():
     offset = max(0, offset - display_results)
     search_images()
 
-# Function to display the thumbnails
-def display_thumbnails(filepaths):
+def display_thumbnails(thumbnails):
     for child in thumbnail_frame.winfo_children():
         child.destroy()
     thumbnail_size = (100, 100)
-    for i, filepath in enumerate(filepaths):
-        print(filepath)
+    for i, thumbnail in enumerate(thumbnails):
+        filepath, names, bboxes = thumbnail
+        print("image",filepath, names, bboxes)
         image = Image.open(filepath)
         image.thumbnail(thumbnail_size)
         photo = ImageTk.PhotoImage(image)
@@ -69,14 +80,12 @@ def display_thumbnails(filepaths):
         col = i % num_columns
         
         label.grid(row=row, column=col, padx=5, pady=5)
-        label.bind("<Button-1>", lambda e, path=filepath: open_image(path))
+        label.bind("<Button-1>", lambda e, path=filepath, names=names, bboxes=bboxes: open_image(path, names=names, bboxes=bboxes))
 
 
 parser = argparse.ArgumentParser(description="Face Database GUI")
 parser.add_argument('--database', default='faces.db', help='Database file name')
 args = parser.parse_args()
-
-# GUI setup
 database = args.database
 
 root = tk.Tk()
@@ -85,7 +94,6 @@ root.title("Face Search")
 main_frame = ttk.Frame(root, padding=10)
 main_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 
-# Entry and buttons
 name_entry = ttk.Entry(main_frame)
 name_entry.grid(row=0, column=0, padx=(0, 5), pady=(0, 5))
 
@@ -101,17 +109,14 @@ prev_button.grid(row=0, column=3, padx=(5, 0), pady=(0, 5))
 next_button = ttk.Button(main_frame, text="Next", command=next_page)
 next_button.grid(row=0, column=4, padx=(5, 0), pady=(0, 5))
 
-# Frame to display the thumbnails
+enable_bbox = tk.BooleanVar(value=False)
+bbox_button = ttk.Checkbutton(main_frame, text="Bbox", variable=enable_bbox)
+bbox_button.grid(row=0, column=5, padx=(5, 0), pady=(0, 5))
+
 canvas = tk.Canvas(root)
 canvas.grid(row=1, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-#scrollbar = ttk.Scrollbar(root, orient="vertical", command=canvas.yview)
-#scrollbar.grid(row=1, column=2, sticky=(tk.N, tk.S))
-
-#canvas.configure(yscrollcommand=scrollbar.set)
-#canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 
 thumbnail_frame = ttk.Frame(canvas)
 canvas.create_window((0, 0), window=thumbnail_frame, anchor="nw")
-#thumbnail_frame.grid(row=1, column=0, pady=(0, 10))
 
 root.mainloop()
